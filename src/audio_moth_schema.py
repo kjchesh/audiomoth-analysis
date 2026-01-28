f6fe1727bc5e0980855bd85aaa480a3b5d4d6ec2
kat <katrina.cheshire94@gmail.com>
2026-01-28 10:23:22 +0000

    remove redundant field specific config

--- src/audio_moth_schema.py
+++ src/audio_moth_schema.py
@@ -4,19 +4,19 @@ from pandera.typing import Series
 
 
 class AudioMothSchema(pa.DataFrameModel):
-    start_s: int = pa.Field(coerce=True)
-    end_s: int = pa.Field(coerce=True)
-    scientific_name: str = pa.Field(coerce=True)
-    common_name: str = pa.Field(coerce=True)
-    confidence: float = pa.Field(ge=0.0, le=1.0, coerce=True)
-    detection_timestamp: Series[pd.Timestamp] = pa.Field(coerce=True, nullable=True)
-    device: str = pa.Field(coerce=True)
-    site: str = pa.Field(coerce=True)
-    location_id: str = pa.Field(coerce=True)
-    habitat: str = pa.Field(coerce=True)
-    w3w: str = pa.Field(coerce=True)
-    deployment_timestamp: Series[pd.Timestamp] = pa.Field(coerce=True, nullable=True)
+    start_s: int = pa.Field()
+    end_s: int = pa.Field()
+    scientific_name: str = pa.Field()
+    common_name: str = pa.Field()
+    confidence: float = pa.Field(ge=0.0, le=1.0)
+    detection_timestamp: Series[pd.Timestamp] = pa.Field(nullable=True)
+    device: str = pa.Field()
+    site: str = pa.Field()
+    location_id: str = pa.Field()
+    habitat: str = pa.Field()
+    w3w: str = pa.Field()
+    deployment_timestamp: Series[pd.Timestamp] = pa.Field(nullable=True)
 
     class Config:  # type: ignore
         coerce = True
-        strict = "filter"  # extra columns not in schema
+        strict = "filter"
